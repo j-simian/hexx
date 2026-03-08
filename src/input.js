0f02acb811dfcbd506b80994110c90a152e3efa7
Jessica Simian <me@jsimian.com>
2026-03-08 14:33:54 +0000

    improve touch handlers

--- src/input.js
+++ src/input.js
@@ -12,43 +12,55 @@ export class InputHandler {
 
   dragging = false;
 
-  onMouseMove(e) {
+  getXY(e) {
+    if (e.changedTouches) return { x: e.changedTouches[0].clientX, y: e.changedTouches[0].clientY };
+    if (e.touches) return { x: e.touches[0].clientX, y: e.touches[0].clientY };
+    return { x: e.clientX, y: e.clientY };
+  }
+
+  onMove(e) {
+    const { x, y } = this.getXY(e);
     if (this.dragging) {
-      this.camera.x += e.clientX - this.lastX;
-      this.camera.y += e.clientY - this.lastY;
+      this.camera.x += x - this.lastX;
+      this.camera.y += y - this.lastY;
     }
-    this.lastX = e.clientX;
-    this.lastY = e.clientY;
-    let { q, r } = pixelToHex(e.clientX - this.camera.x, e.clientY - this.camera.y, 30);
+    this.lastX = x;
+    this.lastY = y;
+    let { q, r } = pixelToHex(x - this.camera.x, y - this.camera.y, this.radius);
     this.hovered = { q, r };
   }
 
   onClick(e) {
-    const worldX = e.clientX - this.camera.x;
-    const worldY = e.clientY - this.camera.y;
+    const { x, y } = this.getXY(e);
+    const worldX = x - this.camera.x;
+    const worldY = y - this.camera.y;
     const { q, r } = pixelToHex(worldX, worldY, this.radius);
     this.onHexClick(q, r);
   }
 
-  onMouseDown(e) {
+  onDown(e) {
+    const { x, y } = this.getXY(e);
     this.dragging = true;
-    this.dragStartX = e.clientX;
-    this.dragStartY = e.clientY;
+    this.dragStartX = x;
+    this.dragStartY = y;
   }
 
-  onMouseUp(e) {
+  onUp(e) {
+    const { x, y } = this.getXY(e);
     this.dragging = false;
-    const dist = Math.hypot(e.clientX - this.dragStartX, e.clientY - this.dragStartY);
+    const dist = Math.hypot(x - this.dragStartX, y - this.dragStartY);
     if (dist < 5) {
       this.onClick(e);
     }
   }
 
-
   registerHandlers(canvas) {
-    canvas.addEventListener("mousemove", (e) => this.onMouseMove(e));
-    canvas.addEventListener("mousedown", (e) => this.onMouseDown(e));
-    canvas.addEventListener("mouseup", (e) => this.onMouseUp(e));
+    canvas.addEventListener("mousemove", (e) => this.onMove(e));
+    canvas.addEventListener("mousedown", (e) => this.onDown(e));
+    canvas.addEventListener("mouseup", (e) => this.onUp(e));
+    canvas.addEventListener("touchstart", (e) => { e.preventDefault(); this.onDown(e); }, { passive: false });
+    canvas.addEventListener("touchmove", (e) => { e.preventDefault(); this.onMove(e); }, { passive: false });
+    canvas.addEventListener("touchend", (e) => { e.preventDefault(); this.onUp(e); }, { passive: false });
   }
 
   constructor(camera, radius, onHexClick) {
